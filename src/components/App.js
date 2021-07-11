ac4a7a5136da767f335e6d4329b15b12c8686e22
farazmoallemi <moallemifaraz@gmail.com>
2021-07-11 15:13:22 -0700

    Added comments and started adding server routes

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Login from './login';
 import createAccount from './createAccount';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import addBookmark from './addBookmark';
 
 function App() {
-    return(
-            <Router>
-                <Switch>
-                    {/* <Route exact path="/" component={MainPage} /> */}
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/newAccount" component={createAccount} />
-                    {/* <Route exact path="/add" component={addBookmark} /> */}
-                </Switch>
-            </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        {/* <Route exact path="/" component={MainPage} /> */}
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/newAccount" component={createAccount} />
+        {/* <Route exact path="/add" component={addBookmark} /> */}
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
